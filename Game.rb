4683193e5d4405a716cdb36c8aa8c0bf65c476e5
App Academy Student <student@appacademy.io>
2014-10-15 18:17:39 -0700

    fixed return bug

--- Game.rb
+++ Game.rb
@@ -52,11 +52,7 @@ class Game
     if !moves.include?(dest) || piece.moved_into_check?(start, dest)
       puts "not a valid move: #{moves}"
       get_valid_move
-<<<<<<< HEAD
       return
-=======
-      return
->>>>>>> castling
     elsif attempting_to_castle?(piece, dest) #castling
       p "get_valid_move attempting to caste"
       if dest[0] > piece.pos[0]
